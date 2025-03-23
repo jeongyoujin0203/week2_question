97310ade2a7b63e57a97c8c0a5d5909727bc0186
youjin <146078042+jeongyoujin0203@users.noreply.github.com>
2025-03-23 21:35:06 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -3,7 +3,7 @@ class PriorityQueue:
     self.heap = []
 
   def push(self, item, priority):
-    entry = (_priority, item)
+    entry = (priority, item)
     self.heap.append(entry)
     self._sift_up(len(self.heap) - 1)
 
